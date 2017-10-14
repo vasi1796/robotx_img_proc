df056fa60f729175e14eacb39b1a9d9ae137337c
RaspPi <vasy1796@gmail.com>
2017-10-14 17:53:20 +0300

    added changes

--- src/open.cpp
+++ src/open.cpp
@@ -3,9 +3,11 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/opencv.hpp>
 #include <raspicam/raspicam_cv.h>
 #include <iostream>
+#include <vector>
 #include "ColorSegmentation.h"
 
 int main( int argc, char** argv )
@@ -18,13 +20,20 @@ int main( int argc, char** argv )
         std::cout <<  "Could not open or find the image" << std::endl ;
         return -1;
     }
+    cv::Mat msk(400,400,CV_8UC1);
+    std::vector<std::vector<cv::Point> > contours;
 	while(cv::waitKey(1) != 'q')
 	{
 		Camera.grab();
 		Camera.retrieve(image);
-		cv::resize(image,image,cv::Size(300,300));
-		imshow("screen",image);
+		cv::resize(image,image,cv::Size(400,400));
+		ColorSegmentation::OthaSpaceThresholdingRGB(image,true,false,false,msk);
+		cv::findContours(msk.clone(),contours,CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE);
+		cv::Mat contourImage(image.size(), CV_8UC3, cv::Scalar(0,0,0));
+		imshow("original",image);
+		imshow("mask",msk);
 	}
+	cv::destroyAllWindows();
 	Camera.release();
     return 0;
 }
